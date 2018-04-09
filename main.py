d5b065d4de0581ca8cb0c45975503d18f10908d8
Ryan Slama <rockinryguy@gmail.com>
2018-04-09 14:52:20 -0400

    Added temp debug statements and minor tweaks

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from evaluate import *
 TODO:
 
 - Implement more UCI features
-  - Iterative Deepening, to allow a customizable thinking time
+    - Iterative Deepening, to allow a customizable thinking time
 
 - Make it harder, better, faster, & stronger
 
@@ -13,100 +13,112 @@ TODO:
 
 # Setup Main Board
 board = chess.Board()
-SEARCH_DEPTH = 4
+SEARCH_DEPTH = 2
 
 
 # Choose a move
 def AI_move(board):
-	bestscore = -999999
-	bestmove = None
+    bestscore = -999999
+    bestmove = None
 
-	for move in board.legal_moves: 
-		board.push(move)
-		score = minimax(SEARCH_DEPTH, board, False, -1000000, 1000000)
-		print(score)
-		if score > bestscore:
-			bestscore = score
-			bestmove = move
-		board.pop()
+    for move in board.legal_moves:
+        board.push(move)
+        print("info depth " + str(SEARCH_DEPTH))
+        score = minimax(SEARCH_DEPTH - 1, board, False, -1000000, 1000000)
+        print("Move Score: " + str(score))
+        if score > bestscore:
+            bestscore = score
+            bestmove = move
+        board.pop()
 
-	board.push(bestmove)
-	return str(bestmove)
+    board.push(bestmove)
+    print("BEST CASE: " + str(bestscore))
+    print(board)
+    return str(bestmove)
 
 
 def minimax(depth, board, isWhite, alpha, beta):
-	if depth == 0:
-		return evaluate_board(board)
-	bestmove = None
-	if not isWhite:
-		bestscore = -999999
-		for move in board.legal_moves: 
-			board.push(move)
-			score = minimax(depth - 1, board, not isWhite, alpha, beta)
-			bestscore = max(bestscore, score)
-			board.pop()
-			alpha = max(alpha, bestscore)
-			if beta <= alpha:
-				return bestscore
-		return bestscore
-	else: 
-		bestscore = 999999
-		for move in board.legal_moves: 
-			board.push(move)
-			score = minimax(depth - 1, board, not isWhite, alpha, beta)
-			bestscore = min(bestscore, score)
-			board.pop()
-			beta = min(beta, bestscore)
-			if beta <= alpha:
-				return bestscore
-		return bestscore
+    if depth == 0:
+        # print(board)
+        print("info currmove "
+              + " ".join(str(mov) for mov in board.move_stack))
+        print ("DEPTH 0 result: " + str(evaluate_board(board)))
+        return evaluate_board(board)
+    bestmove = None
+    if not isWhite:
+        bestscore = -999999
+        for move in board.legal_moves:
+            board.push(move)
+            print(board)
+            score = minimax(depth - 1, board, not isWhite, alpha, beta)
+            bestscore = max(bestscore, score)
+            board.pop()
+            alpha = max(alpha, bestscore)
+            if beta <= alpha:
+                print("Black returning: " + str(bestscore))
+                return bestscore
+        print("Black returning: " + str(bestscore))
+        return bestscore
+    else:
+        bestscore = 999999
+        for move in board.legal_moves:
+            board.push(move)
+            score = minimax(depth - 1, board, not isWhite, alpha, beta)
+            bestscore = min(bestscore, score)
+            board.pop()
+            beta = min(beta, bestscore)
+            if beta <= alpha:
+                print("White returning: " + str(bestscore))
+                return bestscore
+        print("White returning: " + str(bestscore))
+        return bestscore
 
 
 def input_uci():
-	global board
+    global board
 
-	message = input("")
-	with open("log.txt", "a") as log:
-	    log.write(message + '\n')
-	tokens = message.split()
+    message = input("")
+    with open("log.txt", "a") as log:
+        log.write(message + '\n')
+    tokens = message.split()
 
-	if len(tokens) > 0:
-		if tokens[0] == 'uci':
-			print("id name EngineTest")
-			print("id author DTD")
-			print('uciok')
-		elif tokens[0] == 'ucinewgame':
-			board = chess.Board()
-		elif tokens[0] == 'position' and len(tokens) == 2 and tokens[1] == 'startpos':
-			board = chess.Board()
-		elif tokens[0] == 'position' and len(tokens) > 3 \
-		and tokens[1] == 'startpos' and tokens[2] == 'moves':
-			moves = tokens[3:]
-			board = chess.Board()
-			for move in moves:
-				board.push(board.parse_uci(move))
-			print(board)
-		elif tokens[0] == 'position' and len(tokens) > 2 and tokens[1] == 'fen':
-			board = chess.Board(fen=" ".join(tokens[2:]))
-		elif tokens[0] == 'go':
-			print('bestmove ' + AI_move(board))
-		elif tokens[0] == 'stop':
-			print(AI_move(board))
-		elif tokens[0] == 'isready':
-			print('readyok')
-		elif tokens[0] == 'setoption':
-			# TODO: We might need to implement some options
-			pass
-		elif tokens[0] == 'deb':
-			print(board)
-		elif tokens[0] == 'test':
-			print(board.piece_map())	
-		elif tokens[0] == 'quit':
-			quit()
-		else: 
-			print("Unrecognized Command")
+    if len(tokens) > 0:
+        if tokens[0] == 'uci':
+            print("id name EngineTest")
+            print("id author DTD")
+            print('uciok')
+        elif tokens[0] == 'ucinewgame':
+            board = chess.Board()
+        elif tokens[0] == 'position' and len(tokens) == 2 and tokens[1] == 'startpos':
+            board = chess.Board()
+        elif tokens[0] == 'position' and len(tokens) > 3 \
+                and tokens[1] == 'startpos' and tokens[2] == 'moves':
+            moves = tokens[3:]
+            board = chess.Board()
+            for move in moves:
+                board.push(board.parse_uci(move))
+            print(board)
+        elif tokens[0] == 'position' and len(tokens) > 2 and tokens[1] == 'fen':
+            board = chess.Board(fen=" ".join(tokens[2:]))
+        elif tokens[0] == 'go':
+            print('bestmove ' + AI_move(board))
+        elif tokens[0] == 'stop':
+            print(AI_move(board))
+        elif tokens[0] == 'isready':
+            print('readyok')
+        elif tokens[0] == 'setoption':
+            # TODO: We might need to implement some options
+            pass
+        elif tokens[0] == 'deb':
+            print(board)
+        elif tokens[0] == 'test':
+            print(board.piece_map())
+        elif tokens[0] == 'quit':
+            quit()
+        else:
+            print("Unrecognized Command")
 
 
 print(board)
 while True:
-	input_uci()
+    input_uci()
