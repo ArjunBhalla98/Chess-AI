a869bc5780a04e26f4ed28bce5a97db765ffba9e
Arjun Bhalla <arjunbhallasg@gmail.com>
2018-05-09 21:46:49 -0400

    Most of the ML, some bugs still

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import chess
 import math
 from evaluate import *
 import pickle
+import random
 """
 TODO:
 
@@ -15,22 +16,51 @@ TODO:
 # Setup Main Board
 board = chess.Board()
 SEARCH_DEPTH = 4
+EPSILON_VALUE = 0.05
+REWARDS = {"1-0": 1, "0-1": -1, "1/2-1/2": 0}
+LEARNING_RATE = 0.75
+
+board_minimax_scores = {}
+board_minimax_scores_black = {}
+
+
 
 
 # Choose a move
 def AI_move(board):
-    bestscore = -999999
+    bestscore = -99999999999999999
     bestmove = None
+    moves = []
 
     for move in board.legal_moves:
         board.push(move)
         score = minimax(SEARCH_DEPTH - 1, board, True, -1000000, 1000000)
+        moves.append((move, score))
         if score > bestscore:
             bestscore = score
             bestmove = move
         board.pop()
 
+
+    rand_prob = random.random()
+    if rand_prob <= EPSILON_VALUE:
+    	best = random.choice(moves)
+    	bestscore = best[1]
+    	bestmove = best[0]
+
+    b_fen = board.board_fen()
+    if board.turn == chess.WHITE:
+        board_minimax_scores[b_fen] = bestscore
+    else:
+        board_minimax_scores_black[b_fen] = bestscore
+
+
     board.push(bestmove)
+
+    if board.is_game_over():
+        print("YEETH")
+        update_weights(board)
+
     print(board)
     return str(bestmove)
 
@@ -39,7 +69,10 @@ def minimax(depth, board, isWhite, alpha, beta):
     if depth == 0:
         # print("info currmove "
             #   + " ".join(str(mov) for mov in board.move_stack))
-        return evaluate_board(board)
+        if isWhite:
+            return -1*evaluate_board(board)
+        else:
+            return evaluate_board(board)
     bestmove = None
     if not isWhite:  # BLACK
         bestscore = -999999
@@ -105,6 +138,7 @@ def input_uci():
         elif tokens[0] == 'test':
             print(board.piece_map())
         elif tokens[0] == 'quit':
+            print("WE ARE THE CHAMPIONS")
             quit()
         else:
             print("Unrecognized Command")
@@ -112,10 +146,55 @@ def input_uci():
 
 print(board)
 
+
+def update_weights(board):
+
+    with open("position_weights.pickle", "rb") as handle:
+        weights_dict = pickle.load(handle)
+
+    result = board.result()
+    reward_winner = REWARDS[result]
+    reward_loser = -1*reward_winner
+    print(result)
+    
+    alpha = LEARNING_RATE*reward_winner
+    alpha_opposite = LEARNING_RATE*reward_loser - 0.15 # Don't you worry your little head about that number bud
+
+    if result == '1-0': # WHITE WON
+        for board_fen in board_minimax_scores:
+            print(board_fen)
+            brd = board.set_fen(board_fen)
+            for square, piece in brd.piece_map().items():
+                symbol = piece.symbol()
+                weights_dict[symbol][chess.square_rank(square)][chess.square_file(square)] += alpha
+
+        for board_fen in board_minimax_scores_black:
+            brd = board.set_fen(board_fen)
+            for square, piece in brd.piece_map().items():
+                symbol = piece.symbol()
+                weights_dict[symbol][chess.square_rank(square)][chess.square_file(square)] += alpha_opposite
+
+    elif result == '0-1':
+
+        for board_fen in board_minimax_scores_black:
+            print(board_fen)
+            brd = board.set_fen(board_fen)
+            for square, piece in brd.piece_map().items():
+                symbol = piece.symbol()
+                weights_dict[symbol][chess.square_rank(square)][chess.square_file(square)] += alpha
+
+        for board_fen in board_minimax_scores:
+            brd = board.set_fen(board_fen)
+            for square, piece in brd.piece_map().items():
+                symbol = piece.symbol()
+                weights_dict[symbol][chess.square_rank(square)][chess.square_file(square)] += alpha_opposite
+
+    with open("positions_weights.pickle", "wb") as handle:
+        pickle.dump(weights_dict, handle, protocol = pickle.HIGHEST_PROTOCOL)
+
+
+
 ######################################## Monte Carlo Reinforcement Learning METHODS ####################################################
-def init_dicts():
-	with open("state_actions.pickle", "wb") as handle:
-		pickle.dump({}, handle, protocol = pickle.HIGHEST_PROTOCOL)
 
 def q_function(action, board):
 	current_score = evaluate.evaluate_board(board)
